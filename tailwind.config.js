5399102298eb7ba1f53053fa60569f78938ae690
Ansh Singh Sonkhia <anshssonkhia@gmail.com>
2022-10-13 17:15:18 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'fb-green': '#42b72a',
+        'fb-dark-green': '#008600'
+      }
+    },
+  },
+  plugins: [],
+}
